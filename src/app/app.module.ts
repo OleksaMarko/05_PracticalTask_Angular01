58cacace2ac545a20e8c3ed8433528aaeb7bbc21
OleksaMarko <kompotius43@gmail.com>
2023-05-01 21:09:27 +0300

    Add toolbar with task name (‘Angular lecture 1’)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserModule } from './user/user.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, UserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    UserModule,
+    MatToolbarModule,
+  ],
 })
 export class AppModule {}
