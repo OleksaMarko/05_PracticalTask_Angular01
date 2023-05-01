f64e4a4b9ba2f82d3208e659bf3acb941baa4428
OleksaMarko <kompotius43@gmail.com>
2023-05-01 21:36:47 +0300

    Add toolbar with actions (select all, sorting, deleting, search input)

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+  ],
   exports: [UserComponent],
 })
 export class UserModule {}
